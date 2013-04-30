50a21181d041b1a749e4b8f9d5c2b3a4c53c0302
Jan Doberstein <jd@jalogisch.de>
2013-04-30 16:32:05 +0200

    updated to spf13-vim

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="aria2 bashrc gitconfig profile screenrc vim vimrc.before vimrc.after"    # list of files/folders to symlink in homedir
+files="aria2 bashrc gitconfig profile screenrc "    # list of files/folders to symlink in homedir
 
-DO_VIM=yes  #If you want that janus is setup on your system
+DO_VIM=yes  #do the vim walk?
 
 ##########
 
@@ -27,22 +27,44 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
       if [[ -f ~/.$file ]];then
         echo "Moving '.$file' from ~ to $olddir"
         mv ~/.$file $olddir/
       fi
       if [[ -h ~/.$file ]];then
-        echo "Removing old Symlink from .$file"
+        echo "Removing old Symlink from .$file"i
         rm -f ~/.$file
-      fi
+    fi
       echo "Creating symlink to $file in home directory."
       ln -s $dir/$file ~/.$file
 done
 
-# we want to setup janus!
+# switch to spf13-vim
 if [[ $DO_VIM == "yes" ]];then
-  cd $dir/vim
-  rake
+    # just move all found .vim* files to Backupdir and create vimrc.local link
+    for VFILE in $(find ~ -maxdepth 1 -name ".vim*" -type f);do
+        echo "Moving $VFILE to Backupdir"
+        mv $VFILE $olddir
+    done
+    # remove symlinks
+    for VFILE in $(find ~ -maxdepth 1 -name ".vim*" -type l);do
+        echo "remove symlink $VFILE"
+        rm -f $VFILE
+    done
+    # moving directory
+    for VFILE in $(find ~ -maxdepth 1 -name ".vim*" -type d);do
+        echo "Moving $VFILE to Backupdir"
+        mv $VFILE $olddir
+    done
+    if [[ -f $dir/vimrc.local ]];then
+        ln -s $dir/vimrc.local ~/.vimrc.local
+    fi
+    # test for vim.nox on the System and
+    which vim.nox $>/dev/null
+    [ $? -eq 1 ] || echo "you _should_ remove vim.nox on your System"
+    # just change to homedir and start Installation of spf13-vim3
+    cd ~
+    curl http://j.mp/spf13-vim3 -L -o - | sh
 fi
